5118ae21f9c1be3b4d34f7f11d7fa2547d04710e
Adefisayomi Oluwole <adefisayomioluwole@gmail.com>
2021-01-27 04:03:19 +0100

    finished signup front-end

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    env: {
+        NEXT_PUBLIC_APP_ID : "1:116041574980:web:57f8c05fc0fb9d220d6a11",
+        NEXT_PUBLIC_API_KEY : "AIzaSyCvI22CnFsoqACQ2TUqqHM7nP8YZfTx7OY",
+        NEXT_PUBLIC_MESSAGING_SENDER_ID : "116041574980"
+    }
+}
